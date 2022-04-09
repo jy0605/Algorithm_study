50c6148a1a64c31987ccf276ae4ff4e826f514eb
jy0605 <31639823+jy0605@users.noreply.github.com>
2022-04-09 22:06:16 +0900

    ** 2022. 4. 9 - 1260, 두문제 하기!

---
+++ ForCodingTest/Backjun-1260.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForCodingTest
+{
+    internal class Backjun_1260
+    {
+        static void Main()
+        {
+            // 문제 -- 2022. 4. 9 여기 푸는중! 내일 두문제 올리기!
+            // https://www.acmicpc.net/problem/1260
+            string[] inputStr = Console.ReadLine().Split(' ');
+            int nodecnt = int.Parse(inputStr[0]);
+            int linkcnt = int.Parse(inputStr[1]);
+            int startNode = int.Parse(inputStr[2]);
+
+            Dictionary<int, List<int>> nodes = new Dictionary<int, List<int>>();
+
+            List<int> visitedNode = new List<int>();
+
+            for (int i = 0; i < linkcnt; i++)
+            {
+                string[] thisinput = Console.ReadLine().Split(' ');
+                int thisnode = int.Parse(thisinput[0]);
+                int nextnode = int.Parse(thisinput[1]);
+
+                if (! nodes.ContainsKey(thisnode))
+                    nodes.Add(thisnode, new List<int>());   
+                nodes[thisnode].Add(nextnode);
+            }
+
+            
+
+
+        }
+    }
+}
--- ForCodingTest/Programmers-87377.cs
+++ ForCodingTest/Programmers-87377.cs
@@ -8,9 +8,29 @@ namespace ForCodingTest
 {
     internal class Programmers_87377
     {
-        static void Main()
+        static void Main_()
         {
             // 교점에 별 만들기
+            // 각 방정식별로 리스트를 만들어서..(예를들어서 방정수 개수 N이면, 길이 N의 배열을 각각 가짐)
+            // 교차 여부를 0과 1로 구분해서 update
+            // 교차여부는 x를 0->1000까지 증가시키면서 교차여부 확인, 0->-1000까지 감소시키면서 확인
+
+
+            // 그런데 풀이를 찾아보니, 대부분 참고사항으로 풀었다.
+        }
+        
+        public string[] solution(int[,] line)
+        {
+            // item1은 
+            List<Tuple<int,int>> list = new List<Tuple<int, int>>();
+
+            string[] result = new string[line.GetLength(0)];
+
+
+
+
+            return result;
+
         }
     }
 }
