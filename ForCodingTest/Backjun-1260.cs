ef35d33709116f06d1732403a70b72225c4485dd
jy0605 <31639823+jy0605@users.noreply.github.com>
2022-04-11 01:57:38 +0900

    ** 2022. 4. 11 - 첫 번째 시도, foreach only - 두 번째 시도, remove the visited links for BFS - all time out - 세 번째 시도, remove the visited links for DFS too - but it doesn't support the deep copy for dictionary type

--- ForCodingTest/Backjun-1260.cs
+++ ForCodingTest/Backjun-1260.cs
@@ -25,35 +25,68 @@ namespace ForCodingTest
                 int thisnode = int.Parse(thisinput[0]);
                 int nextnode = int.Parse(thisinput[1]);
 
-                if (thisnode > nodecnt)
+                if (thisnode > nodecnt || nextnode > nodecnt)
                     continue;
 
                 if (! nodes.ContainsKey(thisnode))
-                    nodes.Add(thisnode, new List<int>());   
+                    nodes.Add(thisnode, new List<int>());
+                if (! nodes.ContainsKey(nextnode))
+                    nodes.Add(nextnode, new List<int>());
+
                 nodes[thisnode].Add(nextnode);
+                nodes[nextnode].Add(thisnode);
             }
 
             // BFS는 시작노드에서 방문할 수 있는 노드를 먼저 선출력 후 방문 마치면 그 다음 노드로 이동
-            
+            List<int> listDFS = DFS(nodes, startNode);
+            List<int> listBFS = BFS(nodes, startNode);
+
+            Console.WriteLine(String.Join(" ", listDFS));
+            Console.WriteLine(String.Join(" ", listBFS));
         }
 
-        public List<int> BFS(Dictionary<int, List<int>> nodes, int startNode)
+        public static List<int> BFS(Dictionary<int, List<int>> originNodes, int startNode)
         {
             List<int> visitedNode = new List<int>();
+            var nodes = originNodes.ToDictionary(x => x.Key, x => x.Value);
 
             visitedNode.Add(startNode);
+            int stdNode = startNode;
 
-            for (int i = 0; i < nodes.Count; i++)
+            while (visitedNode.Count < nodes.Keys.Count)
             {
+                List<int> thisVisitedNode = new List<int>();
 
+                nodes[stdNode].Sort();
+                int nextStdNode = nodes[stdNode][0];
+                visitedNode.AddRange(nodes[stdNode]);
+                nodes[stdNode].ForEach(node => { 
+                    nodes[node].Remove(stdNode);
+                });
+                nodes.Remove(stdNode);
+
+                stdNode = nextStdNode;
             }
-
+            
             return visitedNode;
 
         }
-        public List<int> DFS(Dictionary<int, List<int>> nodes)
+        public static List<int> DFS(Dictionary<int, List<int>> originNodes, int startNode)
         {
             List<int> visitedNode = new List<int>();
+            var nodes = originNodes.ToDictionary(x => x.Key, x => x.Value);
+
+            visitedNode.Add(startNode);
+            int stdNode = startNode;
+
+            while (visitedNode.Count < nodes.Keys.Count)
+            {
+                nodes[stdNode].Sort();
+                var node = nodes[stdNode][0];
+                if (!visitedNode.Contains(node)) { visitedNode.Add(node); stdNode = node; }
+                nodes[node].Remove(stdNode); 
+                nodes[stdNode].Remove(node);
+            }
 
             return visitedNode;
         }
