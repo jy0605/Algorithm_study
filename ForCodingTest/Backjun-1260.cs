23d9f45a9ad3310f31f0e88ce1f358da627acd70
jy0605 <31639823+jy0605@users.noreply.github.com>
2022-04-12 01:42:30 +0900

    - dictionary copy(2) - 1260 (DFS, BFS) 다른방식으로 접근 필요

--- ForCodingTest/Backjun-1260.cs
+++ ForCodingTest/Backjun-1260.cs
@@ -10,6 +10,7 @@ namespace ForCodingTest
     {
         static void Main()
         {
+            // 2022.4.12 : 다른방식으로 풀어봐야겠다....
             // https://www.acmicpc.net/problem/1260
             string[] inputStr = Console.ReadLine().Split(' ');
             int nodecnt = int.Parse(inputStr[0]);
@@ -36,8 +37,7 @@ namespace ForCodingTest
                 nodes[nextnode].Add(thisnode);
             }
 
-            Dictionary<int, List<int>> secNodes = new Dictionary<int, List<int>>();
-            secNodes = copyDictionary(nodes);
+            Dictionary<int, List<int>> secNodes = copyDictionary(nodes);
 
             // BFS는 시작노드에서 방문할 수 있는 노드를 먼저 선출력 후 방문 마치면 그 다음 노드로 이동
             List<int> listDFS = DFS(nodes, startNode);
@@ -45,17 +45,18 @@ namespace ForCodingTest
 
             Console.WriteLine(String.Join(" ", listDFS));
             Console.WriteLine(String.Join(" ", listBFS));
+
         }
 
-        public static List<int> BFS(Dictionary<int, List<int>> originNodes, int startNode)
+        public static List<int> BFS(Dictionary<int, List<int>> nodes, int startNode)
         {
             List<int> visitedNode = new List<int>();
-            var nodes = originNodes.ToDictionary(x => x.Key, x => x.Value);
+            int initNodeKeyCount = nodes.Keys.Count;
 
             visitedNode.Add(startNode);
             int stdNode = startNode;
 
-            while (visitedNode.Count < nodes.Keys.Count)
+            while (visitedNode.Count < initNodeKeyCount)
             {
                 List<int> thisVisitedNode = new List<int>();
 
@@ -73,15 +74,15 @@ namespace ForCodingTest
             return visitedNode;
 
         }
-        public static List<int> DFS(Dictionary<int, List<int>> originNodes, int startNode)
+        public static List<int> DFS(Dictionary<int, List<int>> nodes, int startNode)
         {
             List<int> visitedNode = new List<int>();
-            var nodes = originNodes.ToDictionary(x => x.Key, x => x.Value);
+            int initNodeKeyCount = nodes.Keys.Count;
 
             visitedNode.Add(startNode);
             int stdNode = startNode;
 
-            while (visitedNode.Count < nodes.Keys.Count)
+            while (visitedNode.Count < initNodeKeyCount)
             {
                 nodes[stdNode].Sort();
                 var node = nodes[stdNode][0];
@@ -95,11 +96,23 @@ namespace ForCodingTest
 
         public static Dictionary<int, List<int>> copyDictionary(Dictionary<int, List<int>> dict)
         {
+            // 새로운 dictionary 객체 생성
             Dictionary<int, List<int>> newDict = new Dictionary<int, List<int>>();
-            List<KeyValuePair<int, List<int>>> pairs = dict.ToList();
-            pairs.ForEach(pair => newDict.Add(pair.Key, pair.Value));
-            //foreach (var key in dict.Keys) -> 이 방법은 return dictionary가 그대로 dict를 참조하므로 fail임
+
+            // 2.5) We can make it like this as well!
+            foreach (var key in dict.Keys)
+                newDict.Add(key, dict[key].ToList());
+
+            // 1) key, dict[key]로 copy해서 return하기 -> 이 방법은 return dictionary가 그대로 dict를 참조하므로 fail임
+            //foreach (var key in dict.Keys) 
             //    newDict.Add(key, dict[key]);
+
+            // 2) KeyValuePair 활용하기.. 그런데 이제 toList()를 곁들인. --> It worked! (O)
+            // List<KeyValuePair<int, List<int>>> pairs = dict.ToList();
+            // pairs.ForEach(pair => newDict.Add(pair.Key, pair.Value)); --> 단, 여기에서 pair의 value를 그대로 가져와서는 안됩니다. toList()로 복사하여 사용합시다.
+            // pairs.ForEach(pair => newDict.Add(pair.Key, pair.Value.ToList()));
+
+
             return newDict;
         }
     }
