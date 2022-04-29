725c869e347ff0ce27bf406e964d1972bd77d3f9
jy0605 <31639823+jy0605@users.noreply.github.com>
2022-04-29 23:18:14 +0900

    ** 2022.4.29 -- programmers n^2 배열자르기 -- still working on it

---
+++ ForCodingTest/Programmers-87390.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForCodingTest
+{
+    internal class Programmers_87390
+    {
+        static void Main()
+        {
+
+        }
+        public static List<long> solution(int n, long left, long right)
+        {
+            List<long> answer = new List<long>();
+
+            long quarantine = 0;
+            long remainder = 0;
+
+            var qandr = Math.DivRem(left, n);
+
+            quarantine = qandr.Quotient + 1;
+            remainder = qandr.Remainder;
+
+            for (long i = left; i < right + 1; i++)
+            {
+                if (remainder + 1 == n)
+                {
+                    quarantine++;
+                    answer.Add(remainder + 1);
+                }
+                else if (remainder < quarantine)
+                    answer.Add(quarantine);
+                else
+                {
+
+                }
+
+            }
+
+
+            return answer;
+        }
+    }
+}
