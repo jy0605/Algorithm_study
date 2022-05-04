e5d0847c019f3f12ec6cfac93bfc9353ad0b15fd
jy0605 <31639823+jy0605@users.noreply.github.com>
2022-05-04 16:34:44 +0900

    ** 2022.5.4
    - Backjun 1110
    - BronzeⅠ

---
+++ ForCodingTest/Backjun-1110.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1_for_pratice
+{
+    class Backjun_1110
+    {
+        static void Main()
+        {
+            // 더하기 사이클
+
+            int originNum = int.Parse(Console.ReadLine());
+            int newNum = -1;
+            int res = 0;
+            
+            while (true)
+            {
+                if(originNum == newNum)
+                    break;
+                else
+                {
+                    if (newNum == -1)
+                        newNum = originNum;
+                    int firstNum = newNum / 10;
+                    int secNum = newNum % 10;
+
+                    int preNewNum = firstNum + secNum;
+
+                    newNum = int.Parse(secNum + "" + preNewNum % 10);
+
+                    res++;
+                }
+
+            }
+
+            Console.Write(res);
+            Console.ReadKey();
+        }
+    }
+}
