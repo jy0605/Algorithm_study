18fd0381a5470b9de7a150437a480f79fcb3b3fa
jy0605 <31639823+jy0605@users.noreply.github.com>
2022-05-18 01:28:38 +0900

    -- 놓친부분까지 포함 다시 풀었음. 이제야 맞았습니다!

--- ForCodingTest/Backjun-1459.cs
+++ ForCodingTest/Backjun-1459.cs
@@ -13,35 +13,27 @@ namespace ForCodingTest
             int costofline = int.Parse(inputs[2]);
             int costofslash = int.Parse(inputs[3]);
 
-            long result = 0;
+            long result1 = 0;
             long result2 = 0;
+            long result3 = 0;
 
-            if (costofslash > ( costofline * 2 ))
-                result = (X + Y) * costofline;
+            // 1. 평행으로만 이동하는 경우
+            result1 = (X + Y) * costofline;
+            // 2. 대각선으로만 이동하는 경우
+            // 홀수의 경우 1을 빼준만큼 더 대각선으로 이동 후 수평이동
+            if ((X + Y) % 2 != 0)
+                result2 = (Math.Max(X, Y) - 1) * costofslash + costofline;
             else
-            {
-                // 대각선으로 최대한 이동
-                result = (X > Y ? Y : X) * costofslash;
-                
-                // 홀수의 경우 1을 빼준만큼 더 대각선으로 이동 후 수평이동
-                if (Math.Abs(X - Y) % 2 != 0)
-                {
-                    result += (Math.Abs(X - Y) - 1) * costofslash;
-                    result += costofline;
-                }
-                // 짝수의 경우 남은 칸만큼 더 대각선이동
-                else
-                    result += Math.Abs(X - Y) * costofslash;
+                result2 = Math.Max(X, Y) * costofslash;
 
-                // 그런데 상기만으로는 최소값이 아닐 경우도 있다.
-                // 예제입력 6번의 경우, 아래의 식이 최소시간이 된다.
-                // 이 경우가 대각선으로 최대한 이동 후 수평이동의 경우이다.
-                // 예제입력 6번 : 10000000 50000000 800 901 => 41010000000
-                result2 = (Math.Min(X, Y) * costofslash) + (Math.Abs(X - Y) * costofline);
+            // 3. 대각선으로 최대한 이동 후 수평이동하는 경우
+            // 그런데 상기만으로는 최소값이 아닐 경우도 있다.
+            // 예제입력 6번의 경우, 아래의 식이 최소시간이 된다.
+            // 이 경우가 대각선으로 최대한 이동 후 수평이동의 경우이다.
+            // 예제입력 6번 : 10000000 50000000 800 901 => 41010000000
+            result3 = (Math.Min(X, Y) * costofslash) + (Math.Abs(X - Y) * costofline);
 
-            }
-
-            Console.WriteLine(Math.Min(result, result2));
+            Console.WriteLine(Math.Min(result1, Math.Min(result2, result3)));
         }
     }
 }
