c7da5103af32db00dc6735c59322bea78f8429dd
jy0605 <31639823+jy0605@users.noreply.github.com>
2022-05-20 23:23:48 +0900

    ** 2022.5.20 -- 1541 -- silver 2

--- ForCodingTest/Backjun-1541.cs
+++ ForCodingTest/Backjun-1541.cs
@@ -1,12 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace ForCodingTest
 {
     internal class Backjun_1541
     {
+
+        static void Main()
+        {
+            string exp = Console.ReadLine();
+            string[] arrExpbyminus = exp.Split('-');
+
+
+            for (int i = 0; i < arrExpbyminus.Length; i++)
+            {
+                int thisNumber = 0;
+                var thisexp = arrExpbyminus[i].Split('+');
+                if (thisexp.Length > 1)
+                {
+                    for (int j = 0; j < thisexp.Length; j++)
+                        thisNumber += int.Parse(thisexp[j]);
+                }
+                else
+                    thisNumber = int.Parse(arrExpbyminus[i]);
+
+                arrExpbyminus[i] = thisNumber.ToString();
+            }
+
+            int result = 0;
+            for (int i = 0; i < arrExpbyminus.Length; i++)
+            {
+                if (i == 0)
+                    result = int.Parse(arrExpbyminus[i]);
+                else
+                    result -= int.Parse(arrExpbyminus[i]);
+            }
+
+            Console.WriteLine(result);
+        }
     }
 }
