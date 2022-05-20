8c90044ca90079fcfaa3d4d5f8d29bbc1510ec83
jy0605 <31639823+jy0605@users.noreply.github.com>
2022-05-20 23:33:08 +0900

    -- 코드길이 simplify

--- ForCodingTest/Backjun-1541.cs
+++ ForCodingTest/Backjun-1541.cs
@@ -17,24 +17,17 @@ namespace ForCodingTest
                 int thisNumber = 0;
                 var thisexp = arrExpbyminus[i].Split('+');
                 if (thisexp.Length > 1)
-                {
                     for (int j = 0; j < thisexp.Length; j++)
                         thisNumber += int.Parse(thisexp[j]);
-                }
                 else
                     thisNumber = int.Parse(arrExpbyminus[i]);
 
                 arrExpbyminus[i] = thisNumber.ToString();
             }
 
-            int result = 0;
-            for (int i = 0; i < arrExpbyminus.Length; i++)
-            {
-                if (i == 0)
-                    result = int.Parse(arrExpbyminus[i]);
-                else
-                    result -= int.Parse(arrExpbyminus[i]);
-            }
+            int result = int.Parse(arrExpbyminus[0]);
+            for (int i = 1; i < arrExpbyminus.Length; i++)
+                result -= int.Parse(arrExpbyminus[i]);
 
             Console.WriteLine(result);
         }
