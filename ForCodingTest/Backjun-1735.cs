a24e373b0ed7f1340512af12a0c31f0da4895b17
jy0605 <31639823+jy0605@users.noreply.github.com>
2022-06-01 23:31:55 +0900

    -- ing

--- ForCodingTest/Backjun-1735.cs
+++ ForCodingTest/Backjun-1735.cs
@@ -8,10 +8,20 @@ namespace ForCodingTest
         {
             // 약수 2,3,5,7
             // 2022.6.1 아직 관련 보는중
-            string[] firstFraction = Console.ReadLine().Split(' ');
-            string[] secondFraction = Console.ReadLine().Split(' ');
+            string[] strFirstFraction = Console.ReadLine().Split(' ');
+            string[] srtSecondFraction = Console.ReadLine().Split(' ');
 
-            
+            int[] firstFraction = Array.ConvertAll(strFirstFraction, int.Parse);
+            int[] secondFraction = Array.ConvertAll(srtSecondFraction, int.Parse);
+
+            int commonDenom = 0;
+
+            if (firstFraction[1] != secondFraction[1])
+                commonDenom = firstFraction[1] * secondFraction[1];
+            else
+                commonDenom = secondFraction[1];
+
+
         }
     }
 }
