de1500dba9a9caedc6887f7cc53a438ce80bce61
jy0605 <31639823+jy0605@users.noreply.github.com>
2022-06-01 23:25:17 +0900

    ** 2022.6.1 -- 1735 아직수행중

--- ForCodingTest/Backjun-1735.cs
+++ ForCodingTest/Backjun-1735.cs
@@ -7,11 +7,11 @@ namespace ForCodingTest
         static void Main()
         {
             // 약수 2,3,5,7
-            
+            // 2022.6.1 아직 관련 보는중
             string[] firstFraction = Console.ReadLine().Split(' ');
             string[] secondFraction = Console.ReadLine().Split(' ');
 
-
+            
         }
     }
 }
