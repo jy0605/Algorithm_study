1a9cb4325f7651c6331bff90eadcf044882f2065
jy0605 <31639823+jy0605@users.noreply.github.com>
2022-06-02 23:17:07 +0900

    -- 유클리드 호제법(Division algorithm) -- 주로 큰 두 수의 최대공약수를 파악하기 힘든 경우에 '나누는 수'와 '나머지'로 나누는 과정을 되풀이하여 나머지가 0이 될 떄, 최대공약수가 나오게 됨.

--- ForCodingTest/Backjun-1735.cs
+++ ForCodingTest/Backjun-1735.cs
@@ -8,6 +8,7 @@ namespace ForCodingTest
         {
             // 약수 2,3,5,7
             // 2022.6.1 아직 관련 보는중
+            // 유클리드호제법
             string[] strFirstFraction = Console.ReadLine().Split(' ');
             string[] srtSecondFraction = Console.ReadLine().Split(' ');
 
@@ -21,7 +22,29 @@ namespace ForCodingTest
             else
                 commonDenom = secondFraction[1];
 
+            int[] resultFraction = new int[2];
 
+            if (commonDenom == secondFraction[1])
+            {
+                resultFraction[0] = firstFraction[0] + secondFraction[0];
+                resultFraction[1] = commonDenom;
+            }
+            else { 
+                resultFraction[0] = firstFraction[0]*secondFraction[1] + secondFraction[0]*firstFraction[1];
+                resultFraction[1] = commonDenom;
+            }
+
+            int gdc = resultFraction[0];
+            int remainder = Math.DivRem(resultFraction[1], resultFraction[0]).Remainder;
+            while (remainder != 0)
+            {
+                int thisremainder = Math.DivRem(gdc, remainder).Remainder;
+                gdc = remainder;
+                remainder = thisremainder;
+            }
+
+            Console.WriteLine(resultFraction[0]/gdc + " " + resultFraction[1]/gdc);
+                
         }
     }
 }
