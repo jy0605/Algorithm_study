d928c497ea78bc0e6b36aebb540987ee7d6d274d
jy0605 <31639823+jy0605@users.noreply.github.com>
2022-06-07 23:54:00 +0900

    ** 2022.6.7 -- bronze 1 -- bruteforce algorithm

---
+++ ForCodingTest/Backjun-1145.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForCodingTest
+{
+    internal class Backjun_1145
+    {
+        static void Main()
+        {
+            // 브루트포스 알고리즘
+            // 순차탐색, DFS, BFS(어떤방법으로든 전체탐색으로 문제를 풀었다면 브루트포스 알고리즘)
+            // 참고 => https://asadal.github.io
+
+            string[] input = Console.ReadLine().Split(' ');
+            int[] arrInt = Array.ConvertAll(input, int.Parse);
+
+            int cnt = 0;
+            int minInt = 1;
+            while (cnt < 3)
+            {
+                cnt = 0;
+                foreach ( int n in arrInt)
+                {
+                    if (minInt % n == 0)
+                        cnt++;
+                    if (cnt == 3)
+                        break;
+                }
+                minInt++;
+            }
+
+            Console.WriteLine(minInt-1);
+
+        }
+    }
+}
--- ForCodingTest/Backjun-1735.cs
+++ ForCodingTest/Backjun-1735.cs
@@ -4,7 +4,7 @@ namespace ForCodingTest
 {
     internal class Backjun_1735
     {
-        static void Main()
+        static void Main_()
         {
             // 약수 2,3,5,7
             // 2022.6.1 아직 관련 보는중
