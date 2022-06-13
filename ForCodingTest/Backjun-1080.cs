4ff6ee8c4f487aebeec7a55b7a293da2f1f5d1cd
jy0605 <31639823+jy0605@users.noreply.github.com>
2022-06-13 23:38:21 +0900

    ** 2022.6.13 -- 1080 -- silver 1

---
+++ ForCodingTest/Backjun-1080.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ForCodingTest
+{
+    internal class Backjun_1080
+    {
+        static void Main()
+        {
+            // 행렬 A를 행렬 B로 바꾸는데 필요한 연산의 횟수의 최솟값을 구하기
+            string[] input = Console.ReadLine().Split(' ');
+            int N = int.Parse(input[0]);
+            int M = int.Parse(input[1]);
+
+            
+        }
+    }
+}
